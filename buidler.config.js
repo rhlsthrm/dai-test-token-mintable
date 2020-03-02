531bdb40039397e0e14df4b7ab6c826573777424
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-03-02 14:41:48 +0100

    Buidler

--- buidler.config.js
+++ buidler.config.js
@@ -2,7 +2,7 @@ usePlugin("@nomiclabs/buidler-etherscan");
 
 module.exports = {
   solc: {
-    version: "0.6.1"
+    version: "0.6.1`"
   },
   etherscan: {
     // The url for the Etherscan API you want to use.
@@ -10,6 +10,6 @@ module.exports = {
     url: "https://api-rinkeby.etherscan.io/api",
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: "2ZAZ2R4NX6Z7522TCTTIIVITV145SWTW63"
+    apiKey: ""
   }
 };
